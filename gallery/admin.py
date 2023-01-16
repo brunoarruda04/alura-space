8383469e0f93ddc9025c27481871f5932a983d27
Bruno A <bruno_arruda_ba@hotmail.com>
2023-01-16 09:38:10 -0300

    feat: admin search options

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Photography
 
-# Register your models here.
+
+class getPhotographs(admin.ModelAdmin):
+    list_display = ("id", "name", "caption")
+    list_display_links = ("name",)
+    search_fields = ("name", )
+
+
+admin.site.register(Photography, getPhotographs)
