ae75382e236b1d859e7a38bc2a905fa2ccf30944
Bruno A <bruno_arruda_ba@hotmail.com>
2023-01-16 09:56:50 -0300

    feat: photo category

--- gallery/admin.py
+++ gallery/admin.py
@@ -6,6 +6,8 @@ class getPhotographs(admin.ModelAdmin):
     list_display = ("id", "name", "caption")
     list_display_links = ("name",)
     search_fields = ("name", )
+    list_filter = ("category",)
+    list_per_page = 10
 
 
 admin.site.register(Photography, getPhotographs)
--- gallery/models.py
+++ gallery/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 
 class Photography(models.Model):
+
+    CATEGORY_OPTIONS = [
+        ("NEBULA", "Nebula"),
+        ("STAR", "Star"),
+        ("GALAXY", "Galaxy"),
+        ("PLANET", "Planet")
+    ]
+
     name = models.CharField(max_length=100, null=False, blank=False)
     caption = models.CharField(max_length=150, null=False, blank=False)
+    category = models.CharField(
+        max_length=50, choices=CATEGORY_OPTIONS, default="")
     description = models.TextField(null=False, blank=False)
     photo = models.CharField(max_length=100, null=False, blank=False)
 
